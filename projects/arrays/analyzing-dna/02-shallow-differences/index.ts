b51acea341c413f94448e136aa26fde0233d43df
Mchumbles <lewis.mcguire92@yahoo.com>
2025-01-25 12:03:39 +0000

    Complete analyzing-dna tasks

--- projects/arrays/analyzing-dna/02-shallow-differences/index.ts
+++ projects/arrays/analyzing-dna/02-shallow-differences/index.ts
@@ -1,2 +1,23 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function shallowDifferences(
+	a: string[],
+	b: string[]
+): (string | undefined)[] | undefined {
+	if (a.length !== b.length) {
+		return undefined;
+	}
+
+	let c: (string | undefined)[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] !== b[i]) {
+			c.push(undefined);
+		} else {
+			c.push(a[i]);
+		}
+	}
+
+	return c;
+}
--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,33 @@
 // Write your deepDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function deepDifferences(
+	a: string[][],
+	b: string[][]
+): ((string | undefined)[] | undefined)[] | undefined {
+	if (a.length !== b.length) {
+		return undefined;
+	}
+
+	let c: ((string | undefined)[] | undefined)[] = [];
+
+	for (let i = 0; i < a.length; i++) {
+		if (a[i].length !== b[i].length) {
+			c.push(undefined);
+			continue;
+		}
+
+		let d: (string | undefined)[] = [];
+
+		for (let j = 0; j < a[i].length; j++) {
+			if (a[i][j] !== b[i][j]) {
+				d.push(undefined);
+			} else {
+				d.push(a[i][j]);
+			}
+		}
+		c.push(d);
+	}
+
+	return c;
+}
