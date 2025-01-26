9a2a51b124560d6e659df0f88e1ea1612df710bb
Mchumbles <lewis.mcguire92@yahoo.com>
2025-01-26 11:48:48 +0000

    Complete javascript-to-typescript

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,19 @@
 // Put your announceMachines function here! ✨
 // See ./original.js for its older JavaScript code.
 
+function announceMachines(announce, ...machines) {
+	let labelledMachines = 0;
+
+	for (let machine of machines) {
+		if (machine.label) {
+			announce(machine.label);
+			labelledMachines++;
+		} else {
+			announce(`Make: ${machine.make}; Model: ${machine.model}`);
+		}
+	}
+
+	return labelledMachines;
+}
+
 module.exports.announceMachines = announceMachines;
--- projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/02-prototypes-to-classes/index.js
@@ -1,5 +1,49 @@
 // Put your Robot and Humanoid classes here! ✨
 // See ./original.js for their older JavaScript code.
 
+const { Robot } = require("./solution");
+
+new (class Robot {
+	constructor(name, abilities) {
+		this.name = name;
+		this.abilities = abilities;
+		this.power = 100;
+	}
+
+	announce() {
+		console.log(`Greetings. I am ${this.name}.`);
+		this.abilities.array.forEach((ability) => {
+			console.log(`I am able to ${ability}.`);
+		});
+	}
+
+	charge(amount) {
+		if (this.power < 100) {
+			this.power = Math.min(this.power + amount, 100);
+			console.log(`Recharged power supplies to ${this.power}.`);
+		}
+	}
+
+	move(distance) {
+		if (this.power < distance) {
+			console.log(`I do not have enough power to move ${distance} units.`);
+		} else {
+			console.log(`Moving ${distance} units.`);
+			this.power -= distance;
+		}
+	}
+})();
+
+class Humanoid extends Robot {
+	constructor(name, abilities, catchphrase) {
+		super(name, abilities);
+		this.catchphrase = catchphrase;
+	}
+	announce() {
+		super.announce();
+		console.log(` > ${this.catchphrase} <`);
+	}
+}
+
 module.exports.Humanoid = Humanoid;
 module.exports.Robot = Robot;
